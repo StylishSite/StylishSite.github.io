49f414e5603819a39809ecbebaa7cf52190dab40
张天华 <zhangtianhua@zhaojiafang.com>
2020-01-14 19:27:04 +0800

    第一次试看效果

---
+++ precache-manifest.0df7d7c9509c7e9169a7ad2b41e06276.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed37e923db58200da8ab693a91472847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70cb495311dde607bf5d",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "040602d872feb98ccb76",
+    "url": "/static/css/main.cbc63b0d.chunk.css"
+  },
+  {
+    "revision": "70cb495311dde607bf5d",
+    "url": "/static/js/2.079fbb50.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.079fbb50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "040602d872feb98ccb76",
+    "url": "/static/js/main.ce13b9cd.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "d0f6b065201505b83b131d071f2ec46c",
+    "url": "/static/media/logo.d0f6b065.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f351edbf3f4b2213c505026d761eba11.js"
+  "/precache-manifest.0df7d7c9509c7e9169a7ad2b41e06276.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
